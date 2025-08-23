bb178d16e128beeb098ed3d8fa577755dc51b515
Arbaaz <arbaaz231122@gmail.com>
2025-08-23 15:06:27 +0530

    first commit

---
+++ routes/orderRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/AdminAuth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+// Admin routes
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+// Payment routes
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
+
+// User routes
+orderRouter.post("/userorders", authUser, userOrders);
+
+export default orderRouter;
